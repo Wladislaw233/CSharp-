4d5dea2d296454f49f6abaf88d49e8c1be62932a
Влад Юрченко <lunatob7@gmail.com>
2023-08-10 14:43:55 +0300

    исправления

---
+++ 3. События/Events/Events/EventHandlerQuantityArrayElementsBiggerN.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Events
+{
+    public class EventHandlerQuantityArrayElementsBiggerN
+    {
+        public void Message(int n)
+        {
+            Console.WriteLine("Количество элементов списка превышает {0}", n);
+        }
+    }
+}
---
+++ 3. События/Events/Events/NumberAnalis.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Events
+{
+    public class NumberAnalis
+    {
+        public delegate void EventHandlerNumberAnalis();
+        public event EventHandlerNumberAnalis numberAnalis;
+
+        public void Analis(int number, int percent)
+        {
+            Random random = new Random();
+
+            while (true)
+            {
+                int newNumber = random.Next(number, number * 2);
+                
+                if ((newNumber/number) * 100 - 100 > percent)
+                {
+                    if (numberAnalis != null) 
+                    {numberAnalis(); break;}
+                    else break;
+                }
+            }
+        }
+
+    }
+}
