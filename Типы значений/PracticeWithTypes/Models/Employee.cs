f6544df7e66d4711bcad52289a25ccfa32f06ad4
Влад Юрченко <lunatob7@gmail.com>
2023-08-10 10:14:27 +0300

    1

---
+++ Типы значений/PracticeWithTypes/Models/Employee.cs
@@ -0,0 +1,8 @@
+﻿namespace Models
+{
+    public class Employee
+    {
+        public Person Person { get; set; }
+        public string Contract { get; set; }
+    }
+}
---
+++ Типы значений/PracticeWithTypes/PracticeWithTypes/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Models;
+
+namespace PracticeWithTypes
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Person person = new Person { FirsName = "Ivan", LastName = "Ivanov", BirthdayDate = DateTime.Now };
+            Employee employee = new Employee { Person = person};
+            CreateEmployeeContarct(employee);
+            Console.WriteLine("{0}", employee.Contract);
+            Currency currency = new Currency();
+            ChangeCurrency(ref currency);
+            Console.WriteLine("Валюта: {0} {1}", currency.Name, currency.Course);
+            Console.ReadLine();
+        }
+
+        static void ChangeCurrency(ref Currency currency)
+        {
+            currency.Name = "RUB";
+            currency.Course = 16.35;
+        }
+        
+        static void CreateEmployeeContarct(Employee employee)
+        {
+            employee.Contract = "Контракт сотрудника: " 
+                                + employee.Person.FirsName + " " 
+                                + employee.Person.LastName + " " +
+                                Convert.ToString(employee.Person.BirthdayDate);
+        }
+    }
+}
