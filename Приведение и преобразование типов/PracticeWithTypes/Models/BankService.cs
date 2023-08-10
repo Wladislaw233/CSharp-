0c44c4761f1be7a7eb55d1086b90e3ee1962cf44
Влад Юрченко <lunatob7@gmail.com>
2023-08-10 10:59:48 +0300

    2

---
+++ Приведение и преобразование типов/PracticeWithTypes/Models/BankService.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+
+namespace Models
+{
+    public class BankService
+    {
+        public void CalculateSalary(double profit, double expenses, List<Employee> employees)
+        {
+            double Salary = (profit - expenses) / employees.Count;
+            foreach (Employee employee in employees)
+            {
+                employee.Salary = (int)Salary;
+            }
+        }
+    }
+}
---
+++ Приведение и преобразование типов/PracticeWithTypes/PracticeWithTypes/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using Models;
+using System.Linq;
+
+namespace PracticeWithTypes
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Person person1 = new Person { FirsName = "Ivan", LastName = "Ivanov", BirthdayDate = DateTime.Now };
+            Person person2 = new Person { FirsName = "Stas", LastName = "Petrov", BirthdayDate = DateTime.Now };
+            Person person3 = new Person { FirsName = "Oleg", LastName = "Zubrov", BirthdayDate = DateTime.Now };
+            
+            Employee[] employees = new[] { new Employee { Person = person1, Owner = true, Contract = person1.FirsName + " " + person1.LastName}
+                                      ,new Employee { Person = person2, Owner = false, Contract = person2.FirsName + " " + person2.LastName}
+                                      ,new Employee { Person = person3, Owner = true, Contract = person3.FirsName + " " + person3.LastName}};
+            
+            List<Employee> Owners = (from employee in employees
+                where employee.Owner == true
+                select employee).ToList();
+            
+            BankService bankService = new BankService();
+            
+            bankService.CalculateSalary(19482.12, 3211.31, Owners);
+            Console.ReadLine();
+        }
+    }
+}
