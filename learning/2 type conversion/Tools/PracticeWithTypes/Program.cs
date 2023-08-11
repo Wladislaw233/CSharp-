adb3e6f43ff7b87674f264a930bc2e5aa80f17d1
Влад Юрченко <lunatob7@gmail.com>
2023-08-11 14:17:51 +0300

    cor. task 2

--- learning/2 type conversion/Tools/PracticeWithTypes/Program.cs
+++ learning/2 type conversion/Tools/PracticeWithTypes/Program.cs
@@ -11,13 +11,13 @@ namespace PracticeWithTypes
     {
         public static void Main(string[] args)
         {
-            Person person1 = new Person { FirsName = "Ivan", LastName = "Ivanov", BirthdayDate = DateTime.Now };
-            Person person2 = new Person { FirsName = "Stas", LastName = "Petrov", BirthdayDate = DateTime.Now };
-            Person person3 = new Person { FirsName = "Oleg", LastName = "Zubrov", BirthdayDate = DateTime.Now };
+            Person person1 = new Person { FirstName = "Ivan", LastName = "Ivanov", DateOfBirth = DateTime.Now };
+            Person person2 = new Person { FirstName = "Stas", LastName = "Petrov", DateOfBirth = DateTime.Now };
+            Person person3 = new Person { FirstName = "Oleg", LastName = "Zubrov", DateOfBirth = DateTime.Now };
             
-            Employee[] employees = new[] { new Employee { Person = person1, Owner = true, Contract = person1.FirsName + " " + person1.LastName}
-                ,new Employee { Person = person2, Owner = false, Contract = person2.FirsName + " " + person2.LastName}
-                ,new Employee { Person = person3, Owner = true, Contract = person3.FirsName + " " + person3.LastName}};
+            Employee[] employees = new[] { new Employee { Person = person1, Owner = true, Contract = person1.FirstName + " " + person1.LastName}
+                ,new Employee { Person = person2, Owner = false, Contract = person2.FirstName + " " + person2.LastName}
+                ,new Employee { Person = person3, Owner = true, Contract = person3.FirstName + " " + person3.LastName}};
             
             List<Employee> Owners = (from employee in employees
                 where employee.Owner == true
@@ -34,7 +34,7 @@ namespace PracticeWithTypes
             bankService.CalculateSalary(profit, experence, Owners);
             foreach (Employee employee in Owners)
             {
-                Console.WriteLine($"У владельца {employee.Person.FirsName} {employee.Person.LastName} зарплата: {employee.Salary} $");
+                Console.WriteLine($"У владельца {employee.Person.FirstName} {employee.Person.LastName} зарплата: {employee.Salary} $");
             }
             Console.WriteLine();
             Console.ReadLine();
