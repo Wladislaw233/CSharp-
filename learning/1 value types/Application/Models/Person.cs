bc646121d257db4a7bf1ca5c2202b72cbdb7268d
Влад Юрченко <lunatob7@gmail.com>
2023-08-11 14:14:26 +0300

    corrections 1

--- learning/1 value types/Application/Models/Person.cs
+++ learning/1 value types/Application/Models/Person.cs
@@ -6,6 +6,6 @@ namespace Models
     {
         public string FirstName { get; set; }
         public string LastName { get; set; }
-        public int Age { get; set; }
+        public DateTime DateOfBirth { get; set; }
     }
 }
--- learning/1 value types/Tools/PracticeWithTypes/Program.cs
+++ learning/1 value types/Tools/PracticeWithTypes/Program.cs
@@ -6,23 +6,21 @@ namespace PracticeWithTypes
     {
         public static void Main(string[] args)
         {
-            Person person = new Person { FirstName = "Ivan", LastName = "Ivanov", Age = 25};
+            Person person = new Person { FirstName = "Ivan", LastName = "Ivanov", DateOfBirth = DateTime.Now};
             Employee employee = new Employee { Person = person};
-            UpdateEmployeeContarct(employee);
+            UpdateEmployeeContract(employee);
             Console.WriteLine("{0}", employee.Contract);
             Currency currency = new Currency();
             ChangeCurrency(ref currency);
             Console.WriteLine("Валюта: {0} {1}", currency.Name, currency.Course);
             Console.ReadLine();
         }
-
         static void ChangeCurrency(ref Currency currency)
         {
             currency.Name = "RUB";
             currency.Course = 16.35;
         }
-        
-        static void UpdateEmployeeContarct(Employee employee)
+        static void UpdateEmployeeContract(Employee employee)
         {
             employee.Contract = "Контракт сотрудника: " 
                                 + employee.Person.FirstName + " " 
