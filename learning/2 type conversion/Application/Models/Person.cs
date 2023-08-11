4dec88b99cac23ea4bae58d9812e3c8fcede594b
Влад Юрченко <lunatob7@gmail.com>
2023-08-11 11:12:10 +0300

    new solution

---
+++ learning/2 type conversion/Application/Models/Person.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Models
+{
+    public class Person
+    {
+        public string FirsName { get; set; }
+        public string LastName { get; set; }
+        public DateTime BirthdayDate { get; set; }
+    }
+}
---
+++ learning/3 events/Application/Services/EventHandlerEmptyArray.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Services
+{
+    public class EventHandlerEmptyArray
+    {
+        public void Message()
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Список пуст!");
+            Console.ResetColor();
+        }
+    }
+}
