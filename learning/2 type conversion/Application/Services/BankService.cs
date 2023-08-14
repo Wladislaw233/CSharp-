7fb291651f2278deb00fb1d093f2c4fe1c9743f3
Влад Юрченко <lunatob7@gmail.com>
2023-08-14 10:59:43 +0300

    Update BankService.cs

--- learning/2 type conversion/Application/Services/BankService.cs
+++ learning/2 type conversion/Application/Services/BankService.cs
@@ -6,8 +6,8 @@ namespace Services
     {
         public void CalculateSalaryOfBankOwners(double profit, double expenses, List<Employee> owners)
         {
-            var Salary = (profit - expenses) / owners.Count;
-            foreach (var employee in owners) employee.Salary = (int)Salary;
+            double salary = (profit - expenses) / owners.Count;
+            foreach (var employee in owners) employee.Salary = (int)salary;
         }
     }
 }
