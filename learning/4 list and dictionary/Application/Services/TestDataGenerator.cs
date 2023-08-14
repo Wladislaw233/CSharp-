d855274ce21dcb04ecc9a59bef7b7c651c0bbe0a
Влад Юрченко <lunatob7@gmail.com>
2023-08-14 13:49:51 +0300

    task 4

--- learning/4 list and dictionary/Application/Services/TestDataGenerator.cs
+++ learning/4 list and dictionary/Application/Services/TestDataGenerator.cs
@@ -4,109 +4,123 @@ namespace Services;
 
 public class TestDataGenerator
 {
-    private static Random random = new Random();
-    private static List<string> names = new List<string>
+    private static readonly Random Rnd = new();
+
+    private static readonly List<string> Names = new()
     {
         "Eli", "Lucius", "Stephan", "Kirby", "Barbra", "Ralph", "Karla",
         "Taylor", "Benita", "Lisa", "Roy", "Buck", "Rod", "Colette", "Quinn"
     };
-    private static List<string> surnames = new List<string>
+
+    private static readonly List<string> Surnames = new()
     {
         "Ali", "Stout", "Christian", "Bright", "Shelton", "Tapia", "Carr",
         "Bryant", "Romero", "Zhang", "Barton", "Paul", "Berger", "Chandler", "Collins"
     };
-    private static List<string> streets = new List<string>
+
+    private static readonly List<string> Streets = new()
     {
         ", ул. Ленина", ", ул. Мира", ", ул. 25 Октября", ", ул. Мая", ", ул. Лесная", ", пер. Энгельса"
     };
-    private static List<string> cities = new List<string>
+
+    private static readonly List<string> Cities = new()
     {
         "Тирасполь", "Рыбница", "Григориополь", "Дубоссары", "Каменка"
     };
-    private static List<string> emailDomains = new List<string>
+
+    private static readonly List<string> EmailDomains = new()
     {
         "@gmail.com", "@yahoo.com", "@outlook.com", "@hotmail.com", "@example.com", "@testmail.com"
     };
+
     private static string GenerateRandomPhoneNumber()
     {
-        return string.Format("{0:8}",random.Next(10000000, 99999999));
+        return string.Format("{0:8}", Rnd.Next(10000000, 99999999));
     }
+
     private static string GenerateRandomFirstName()
     {
-        return names[random.Next(names.Count)];
+        return Names[Rnd.Next(Names.Count)];
     }
+
     private static string GenerateRandomLastName()
     {
-        return surnames[random.Next(surnames.Count)];
+        return Surnames[Rnd.Next(Surnames.Count)];
     }
+
     private static string GenerateRandomEmail(string firstName, string lastName)
     {
-        return firstName + lastName + emailDomains[random.Next(emailDomains.Count)];
+        return firstName + lastName + EmailDomains[Rnd.Next(EmailDomains.Count)];
     }
+
     private static string GenerateRandomAddress()
     {
-        return cities[random.Next(cities.Count)] + streets[random.Next(streets.Count)];
+        return Cities[Rnd.Next(Cities.Count)] + Streets[Rnd.Next(Streets.Count)];
     }
+
     private static DateTime GenerateRandomDateOfBirth()
     {
-        int year = random.Next(1900, DateTime.Now.Year - 18);
-        int month = random.Next(1, 13);
-        int day = random.Next(1, DateTime.DaysInMonth(year, month) + 1);
+        var year = Rnd.Next(1900, DateTime.Now.Year - 18);
+        var month = Rnd.Next(1, 13);
+        var day = Rnd.Next(1, DateTime.DaysInMonth(year, month) + 1);
         return new DateTime(year, month, day);
     }
+
+    private static Client GenerateRandomClient()
+    {
+        var firstName = GenerateRandomFirstName();
+        var lastName = GenerateRandomLastName();
+        return new Client
+        {
+            FirstName = firstName,
+            LastName = lastName,
+            Address = GenerateRandomAddress(),
+            Email = GenerateRandomEmail(firstName, lastName),
+            PhoneNumber = GenerateRandomPhoneNumber(),
+            Age = Rnd.Next(18, 70)
+        };
+    }
+
+    private static Employee GenerateRandomEmployee()
+    {
+        var firstName = GenerateRandomFirstName();
+        var lastName = GenerateRandomLastName();
+        var dateOfBirth = GenerateRandomDateOfBirth();
+        return new Employee
+        {
+            FirstName = firstName,
+            LastName = lastName,
+            Address = GenerateRandomAddress(),
+            DateOfBirth = dateOfBirth,
+            Contract = firstName + " " + lastName + ", дата рождения: " + dateOfBirth,
+            Email = GenerateRandomEmail(firstName, lastName),
+            Owner = false,
+            PhoneNumber = GenerateRandomPhoneNumber(),
+            Salary = Rnd.Next(10000, 99999)
+        };
+    }
+
     public static List<Client> GenerateListWitchThousandBankClients()
     {
-        List<Client> listBankClients = new List<Client>();
-        string firstName;
-        string lastName;
-        for (int index = 0; index < 1000; index++)
-        {
-            firstName = GenerateRandomFirstName();
-            lastName = GenerateRandomLastName();
-            listBankClients.Add(new Client
-            {
-                FirstName = firstName,
-                LastName = lastName,
-                Address = GenerateRandomAddress(), 
-                Email = GenerateRandomEmail(firstName, lastName),
-                PhoneNumber = GenerateRandomPhoneNumber(),
-                Age = random.Next(18, 70)
-            });
-        }
+        var listBankClients = new List<Client>();
+        for (var index = 0; index < 1000; index++) listBankClients.Add(GenerateRandomClient());
+
         return listBankClients;
     }
 
     public static Dictionary<string, Client> GenerateDictionaryWithBankClients(List<Client> listBankClients)
     {
-        Dictionary<string, Client> dictionaryBankClients = new Dictionary<string, Client>();
+        var dictionaryBankClients = new Dictionary<string, Client>();
         foreach (var bankClient in listBankClients) dictionaryBankClients[bankClient.PhoneNumber] = bankClient;
+        dictionaryBankClients.Add("00000000", GenerateRandomClient());
         return dictionaryBankClients;
     }
 
     public static List<Employee> GenerateListWithThousandEmployees()
     {
-        List<Employee> listBankEmployees = new List<Employee>();
-        string firstName;
-        string lastName;
-        DateTime dateOfBirth;
-        for (int index = 0; index < 1000; index++)
-        {
-            firstName = GenerateRandomFirstName();
-            lastName = GenerateRandomLastName();
-            dateOfBirth = GenerateRandomDateOfBirth();
-            listBankEmployees.Add(new Employee
-            {
-                FirstName = firstName,
-                LastName = lastName,
-                Address = GenerateRandomAddress(),
-                DateOfBirth = dateOfBirth,
-                Contract = firstName + " " + lastName + ", дата рождения: " + dateOfBirth,
-                Email = GenerateRandomEmail(firstName, lastName),
-                Owner = false,
-                PhoneNumber = GenerateRandomPhoneNumber(),
-                Salary = random.Next(10000, 99999)
-            });
-        }
+        var listBankEmployees = new List<Employee>();
+        for (var index = 0; index < 1000; index++) listBankEmployees.Add(GenerateRandomEmployee());
+
         return listBankEmployees;
     }
 }
--- learning/4 list and dictionary/Tools/PracticeWithListAndDictionary/Program.cs
+++ learning/4 list and dictionary/Tools/PracticeWithListAndDictionary/Program.cs
@@ -9,27 +9,67 @@ namespace PracticeWithListAndDictionary
     {
         public static void Main(string[] args)
         {
-            TestDataGenerator services = new TestDataGenerator();
+            var services = new TestDataGenerator();
+            // 1.a
+            var listBankClients = TestDataGenerator.GenerateListWitchThousandBankClients();
+            // 1.б
+            var dictionaryBankClients = TestDataGenerator.GenerateDictionaryWithBankClients(listBankClients);
+            // 1.в
+            var listBankEmployees = TestDataGenerator.GenerateListWithThousandEmployees();
 
-            List<Client> listBankClients = TestDataGenerator.GenerateListWitchThousandBankClients();
-            Dictionary<string, Client> dictionaryBankClients = TestDataGenerator.GenerateDictionaryWithBankClients(listBankClients);
-            List<Employee> listBankEmployees = TestDataGenerator.GenerateListWithThousandEmployees();
+            var phoneNumber = listBankClients[501].PhoneNumber;
+            var stopWatch = new Stopwatch();
             
-            string phoneNumber = listBankClients[501].PhoneNumber;
-            Stopwatch stopWatch = new Stopwatch(); 
-            
+            // 2.а
             stopWatch.Start();
-            Client foundClientInList = listBankClients.Find(bankClient => bankClient.PhoneNumber == phoneNumber);
+            var foundClientInListByPhone = listBankClients.Find(bankClient => bankClient.PhoneNumber == phoneNumber);
             stopWatch.Stop();
-            Console.WriteLine($"Время на поиск клиента ({foundClientInList.FirstName} {foundClientInList.LastName}) в списке по номеру телефона - {stopWatch.Elapsed.Milliseconds} мс.");
-            
+            Console.WriteLine(
+                $"Время на поиск клиента ({foundClientInListByPhone.FirstName} {foundClientInListByPhone.LastName}) " +
+                $"в списке по номеру телефона - {stopWatch.Elapsed.TotalMilliseconds} мс.");
+
+            // 2.б
             stopWatch.Start();
-            Client foundClientInDictionary;
-            bool clientFound = dictionaryBankClients.TryGetValue(phoneNumber, out foundClientInDictionary);
+            var clientFound = dictionaryBankClients.TryGetValue(phoneNumber, out var foundClientInDictionaryByPhone);
             stopWatch.Stop();
-            Console.WriteLine($"Время на поиск клиента (" + (clientFound == true ? foundClientInDictionary.FirstName + " " + foundClientInDictionary.LastName : "Клиент не найден") + ") в словаре по номеру телефона - {stopWatch.Elapsed.Milliseconds} мс.");
+            Console.WriteLine("Время на поиск клиента (" +
+                              (clientFound
+                                  ? foundClientInDictionaryByPhone.FirstName + " " + foundClientInDictionaryByPhone.LastName
+                                  : "Клиент не найден") +
+                              $") в словаре по номеру телефона - {stopWatch.Elapsed.TotalMilliseconds} мс.");
             
+            // 2.в
+            var numberOfFoundClientsInListByAge = listBankClients.Where(bankClient => bankClient.Age < 42).Count();
+            Console.WriteLine(
+                $"Количество клиентов в списке возраст которых меньше 42 - {numberOfFoundClientsInListByAge}");
+
+            // 2.г
+            var foundEmployeeInListWithMinimumSalary = listBankEmployees.MinBy(bankEmployee => bankEmployee.Salary);
+            Console.WriteLine(
+                $"Сотрудник с самой маленькой зарплатой - {foundEmployeeInListWithMinimumSalary.FirstName} {foundEmployeeInListWithMinimumSalary.LastName}, {foundEmployeeInListWithMinimumSalary.Salary} $");
             
+            // 2.д.1
+            stopWatch.Start();
+            var foundDictionaryElementWithLastClient =
+                dictionaryBankClients.FirstOrDefault(dictionaryElement => dictionaryElement.Key == "00000000");
+            stopWatch.Stop();
+            Console.WriteLine("Время на поиск последнего клиента (" +
+                              (foundDictionaryElementWithLastClient.Key != null
+                                  ? foundDictionaryElementWithLastClient.Value.FirstName + " " +
+                                    foundDictionaryElementWithLastClient.Value.LastName
+                                  : "Клиент не найден") +
+                              $") в словаре методом FirstOrDefault - {stopWatch.Elapsed.TotalMilliseconds} мс.");
+            
+            // 2.д.2
+            stopWatch.Start();
+            dictionaryBankClients.TryGetValue("00000000", out var foundDictionaryElementWithLastClientByKey);
+            stopWatch.Stop();
+            Console.WriteLine("Время на поиск последнего клиента (" +
+                              (foundDictionaryElementWithLastClientByKey != null
+                                  ? foundDictionaryElementWithLastClientByKey.FirstName + " " + foundDictionaryElementWithLastClientByKey.LastName
+                                  : "Клиент не найден") +
+                              $") в словаре по ключу - {stopWatch.Elapsed.TotalMilliseconds} мс.");
+
             Console.ReadLine();
         }
     }
