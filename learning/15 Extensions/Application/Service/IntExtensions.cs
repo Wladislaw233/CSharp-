f305339f03b23023a880654525760bbdd9efcca7
Влад Юрченко <lunatob7@gmail.com>
2023-08-23 14:54:16 +0300

    Task 15

---
+++ learning/15 Extensions/Application/Service/IntExtensions.cs
@@ -0,0 +1,24 @@
+﻿namespace Service;
+
+public static class IntExtensions
+{
+    public static TimeSpan Seconds(this int value)
+    {
+        return TimeSpan.FromSeconds(value);
+    }
+
+    public static TimeSpan Minutes(this int value)
+    {
+        return TimeSpan.FromMinutes(value);
+    }
+
+    public static TimeSpan Hours(this int value)
+    {
+        return TimeSpan.FromHours(value);
+    }
+
+    public static TimeSpan Days(this int value)
+    {
+        return TimeSpan.FromDays(value);
+    }
+}
---
+++ learning/15 Extensions/Tools/PracticeWithExtensions/Program.cs
@@ -0,0 +1,17 @@
+﻿using Service;
+
+namespace PracticeWithExtensions;
+
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+        int seconds = 5;
+        TimeSpan timeSpan = seconds.Seconds();
+        Console.WriteLine($"TimeSpan: {timeSpan}");
+
+        int minutes = 10;
+        timeSpan = minutes.Minutes();
+        Console.WriteLine($"TimeSpan: {timeSpan}"); 
+    }
+}
